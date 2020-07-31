7f451c7b959dac2563c2cf6de3db5c4cca569def
SoraiaC <soraia.sosi@gmail.com>
2020-07-30 20:25:59 -0400

    Adicionar arquivos de projeto.

---
+++ MyMovies/Data/MyMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyMovies.Movies;
+
+namespace MyMovies.Data
+{
+    public class MyMoviesContext : DbContext
+    {
+        public MyMoviesContext (DbContextOptions<MyMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyMovies.Movies.Movie> Movie { get; set; }
+    }
+}
---
+++ MyMovies/Movies/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMovies.Movies
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int PublicationYear { get; set; }
+        public int Pages { get; set; }
+        public int Total { get; set; }
+    }
+}
